a084884a6fe245ef00a009cd92eddad0c0ef17f7
Johnny Rocket <0.snilcy@gmail.com>
2020-10-14 16:58:09 +0300

    Updated paths

--- src/components/LogContrller.ts
+++ src/components/LogContrller.ts
@@ -2,118 +2,118 @@ import vscode, { workspace, window } from 'vscode';
 
 import Log from './Log';
 import Decorator from './ui/Decorator';
-import sidebar from './ui/sidebar/Sidebar';
+import sidebar from './ui/Sidebar/Sidebar';
 import logger from './Logger';
 import settings from './Settings';
 
 class LogController {
-	private logs: Log[] = [];
-	private decorator = new Decorator();
-	private listeners: vscode.Disposable[] = [];
-	private isLoad = false;
-	private sidebar = sidebar;
+  private logs: Log[] = [];
+  private decorator = new Decorator();
+  private listeners: vscode.Disposable[] = [];
+  private isLoad = false;
+  private sidebar = sidebar;
 
-	/**
-	 * Adds log to array and UI if the page has been loaded
-	 */
-	log = (log: Log) => {
-		this.logs.push(log);
-		logger.log('log', {
-			logsLength: this.logs.length,
-			isLoad: this.isLoad,
-			logTitle: log.previewTitle,
-		});
+  /**
+   * Adds log to array and UI if the page has been loaded
+   */
+  log = (log: Log) => {
+    this.logs.push(log);
+    logger.log('log', {
+      logsLength: this.logs.length,
+      isLoad: this.isLoad,
+      logTitle: log.previewTitle,
+    });
 
-		if (settings.editor.treeViewMode && !this.isLoad) {
-			return;
-		}
+    if (settings.editor.treeViewMode && !this.isLoad) {
+      return;
+    }
 
-		this.sidebar.add(this.logs);
-		this.decorator.add([log]);
-	};
+    this.sidebar.add(this.logs);
+    this.decorator.add([log]);
+  };
 
-	/**
-	 * Clear the log array and wait "load"
-	 */
-	update = () => {
-		this.logs = [];
+  /**
+   * Clear the log array and wait "load"
+   */
+  update = () => {
+    this.logs = [];
 
-		logger.log('update', {
-			isLoad: this.isLoad,
-			logsLength: this.logs.length,
-		});
+    logger.log('update', {
+      isLoad: this.isLoad,
+      logsLength: this.logs.length,
+    });
 
-		this.isLoad = false;
+    this.isLoad = false;
 
-		if (settings.editor.treeViewMode) {
-			this.sidebar.update();
-		} else {
-			this.sidebar.clear();
-		}
+    if (settings.editor.treeViewMode) {
+      this.sidebar.update();
+    } else {
+      this.sidebar.clear();
+    }
 
-		this.decorator.reset();
-	};
+    this.decorator.reset();
+  };
 
-	/**
-	 * Fired, when the page has loaded
-	 */
-	load = () => {
-		logger.log('load', {
-			logsLength: this.logs.length,
-		});
+  /**
+   * Fired, when the page has loaded
+   */
+  load = () => {
+    logger.log('load', {
+      logsLength: this.logs.length,
+    });
 
-		this.sidebar.emit('load', this.logs);
-		this.decorator.add(this.logs);
-		this.isLoad = true;
-	};
+    this.sidebar.emit('load', this.logs);
+    this.decorator.add(this.logs);
+    this.isLoad = true;
+  };
 
-	/**
-	 * Fires, when ext has started
-	 */
-	addListeners() {
-		logger.log();
+  /**
+   * Fires, when ext has started
+   */
+  addListeners() {
+    logger.log();
 
-		workspace.onDidChangeTextDocument(
-			() => {
-				if (this.isLoad) {
-					this.decorator.add(this.logs);
-				}
-			},
-			null,
-			this.listeners
-		);
+    workspace.onDidChangeTextDocument(
+      () => {
+        if (this.isLoad) {
+          this.decorator.add(this.logs);
+        }
+      },
+      null,
+      this.listeners
+    );
 
-		window.onDidChangeActiveTextEditor(
-			() => this.decorator.add(this.logs),
-			null,
-			this.listeners
-		);
+    window.onDidChangeActiveTextEditor(
+      () => this.decorator.add(this.logs),
+      null,
+      this.listeners
+    );
 
-		workspace.onWillSaveTextDocument(
-			() => {
-				this.isLoad = false;
-			},
-			null,
-			this.listeners
-		);
+    workspace.onWillSaveTextDocument(
+      () => {
+        this.isLoad = false;
+      },
+      null,
+      this.listeners
+    );
 
-		this.sidebar.isReady = true;
-	}
+    this.sidebar.isReady = true;
+  }
 
-	/**
-	 * Fires, when ext has stopped
-	 */
-	removeListeners() {
-		logger.log();
+  /**
+   * Fires, when ext has stopped
+   */
+  removeListeners() {
+    logger.log();
 
-		this.logs = [];
-		this.isLoad = false;
-		this.sidebar.clear();
-		this.decorator.reset();
+    this.logs = [];
+    this.isLoad = false;
+    this.sidebar.clear();
+    this.decorator.reset();
 
-		this.listeners.forEach((listener) => listener.dispose());
-		this.sidebar.isReady = false;
-	}
+    this.listeners.forEach((listener) => listener.dispose());
+    this.sidebar.isReady = false;
+  }
 }
 
 export default new LogController();
--- src/extension.ts
+++ src/extension.ts
@@ -3,45 +3,60 @@ import logger from './components/Logger';
 import settings from './components/Settings';
 import extension from './components/Extension';
 import statusBar from './components/ui/StatusBar';
-import ArgTreeItem from './components/ui/sidebar/tree-items/ArgTreeItem';
-import LogTreeItem from './components/ui/sidebar/tree-items/LogTreeItem';
-import sidebar from './components/ui/sidebar/Sidebar';
+import ArgTreeItem from './components/ui/Sidebar/tree-items/ArgTreeItem';
+import LogTreeItem from './components/ui/Sidebar/tree-items/LogTreeItem';
+import sidebar from './components/ui/Sidebar/Sidebar';
 
 export async function activate(context: vscode.ExtensionContext) {
-	try {
-		logger.init(context);
-		extension.init();
+  try {
+    logger.init(context);
+    extension.init();
 
-		context.subscriptions.push(
-			statusBar.bar,
-			window.registerTreeDataProvider('browser-console-view', sidebar),
+    context.subscriptions.push(
+      statusBar.bar,
+      window.registerTreeDataProvider('browser-console-view', sidebar),
 
-			vscode.commands.registerCommand('browser-console.commands', extension.showCommands),
-			vscode.commands.registerCommand('browser-console.start', extension.startExtension),
-			vscode.commands.registerCommand('browser-console.stop', extension.stopExtensin),
-			vscode.commands.registerCommand('browser-console.reload', extension.reload),
-			vscode.commands.registerCommand('browser-console.toggleViewMode', () => {
-				settings.update({ treeViewMode: !settings.editor.treeViewMode });
-			}),
+      vscode.commands.registerCommand(
+        'browser-console.commands',
+        extension.showCommands
+      ),
+      vscode.commands.registerCommand(
+        'browser-console.start',
+        extension.startExtension
+      ),
+      vscode.commands.registerCommand(
+        'browser-console.stop',
+        extension.stopExtensin
+      ),
+      vscode.commands.registerCommand(
+        'browser-console.reload',
+        extension.reload
+      ),
+      vscode.commands.registerCommand('browser-console.toggleViewMode', () => {
+        settings.update({ treeViewMode: !settings.editor.treeViewMode });
+      }),
 
-			vscode.commands.registerCommand(
-				'browser-console.restart',
-				extension.restartExtension
-			),
-			vscode.commands.registerCommand(
-				'browser-console.showLine',
-				(treeItem: ArgTreeItem | LogTreeItem) =>
-					extension.showLine(treeItem.log.originalPosition)
-			),
-			vscode.commands.registerCommand('browser-console.toggleEnumerable', () => {
-				settings.update({ showEnumerable: !settings.editor.showEnumerable });
-			})
-		);
-	} catch (err) {
-		extension?.emit('error', err);
-	}
+      vscode.commands.registerCommand(
+        'browser-console.restart',
+        extension.restartExtension
+      ),
+      vscode.commands.registerCommand(
+        'browser-console.showLine',
+        (treeItem: ArgTreeItem | LogTreeItem) =>
+          extension.showLine(treeItem.log.originalPosition)
+      ),
+      vscode.commands.registerCommand(
+        'browser-console.toggleEnumerable',
+        () => {
+          settings.update({ showEnumerable: !settings.editor.showEnumerable });
+        }
+      )
+    );
+  } catch (err) {
+    extension?.emit('error', err);
+  }
 }
 
 export async function deactivate() {
-	await extension.stopExtensin();
+  await extension.stopExtensin();
 }
