8462e1855daee0a03d12ac829e41b66206c9ea77
Johnny Rocket <0.snilcy@gmail.com>
2020-11-10 14:52:14 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,9 @@ const config = {
     new CopyPlugin({
       patterns: [
         path.resolve(__dirname, './node_modules/source-map/lib/mappings.wasm'),
+        path.resolve(__dirname, './assets/icon.png'),
         {
-          from: path.resolve(__dirname, './assets/'),
+          from: path.resolve(__dirname, './assets/img'),
           to: './img/',
         },
       ],
